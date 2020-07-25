fd9b4582e755d07c927022c3c4ca4c4d1e829811
Riddhi Delvadia <riddhidel@gmail.com>
2020-07-25 16:04:03 +0530

    add numbers

---
+++ numbers.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Numbers and arithemetic opertaions in python
+
+# In[1]:
+
+
+2+2
+
+
+# In[2]:
+
+
+2*2
+
+
+# In[3]:
+
+
+2-2
+
+
+# In[4]:
+
+
+2/2
+
+
+# In[5]:
+
+
+2%2
+
+
+# In[7]:
+
+
+2**3
+
+
+# In[9]:
+
+
+9**0.5
+
+
+# In[10]:
+
+
+a=5
+
+
+# In[11]:
+
+
+a=a+a
+
+
+# In[12]:
+
+
+a
+
+
+# In[13]:
+
+
+3//2
+
+
+# In[ ]:
+
+
+
+
